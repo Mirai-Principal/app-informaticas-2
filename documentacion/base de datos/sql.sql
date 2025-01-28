f252bca7b157b04aedaf07eb09f77540e438e82b
Darwin Bayas <tidomar@gmail.com>
2025-01-28 08:15:03 -0500

    correccion historias

--- documentacion/base de datos/sql.sql
+++ documentacion/base de datos/sql.sql
@@ -34,16 +34,18 @@ create table efacture_repo.fraccion_basica_desgravada (
 create table efacture_repo.categorias (
    cod_categoria        serial               not null,
    cod_fraccion_basica  int4                 not null,
-   categoria            varchar(50)          not null UNIQUE,
+   categoria            varchar(50)          not null,
    descripcion_categoria text                 null,
    cant_fraccion_basica numeric(6,3)         not null
       constraint ckc_cant_fraccion_bas_categori check (cant_fraccion_basica >= 0),
    created_at           timestamp            not null default current_timestamp,
    updated_at           timestamp            null,
    deleted_at           timestamp            null,
+   CONSTRAINT unique_cod_fraccion_categoria UNIQUE (cod_fraccion_basica, categoria),
    constraint pk_categorias primary key (cod_categoria),
    constraint fk_categori_reference_fraccion foreign key (cod_fraccion_basica)
       references efacture_repo.fraccion_basica_desgravada (cod_fraccion_basica)
+      on delete cascade on update cascade
 );
 
 /*==============================================================*/
@@ -119,8 +121,7 @@ create table efacture_repo.detalles (
    deleted_at           timestamp            null,
    constraint pk_detalles primary key (cod_detalle),
    constraint fk_detalles_reference_categori foreign key (cod_categoria)
-      references efacture_repo.categorias (cod_categoria)
-      on delete cascade on update cascade,
+      references efacture_repo.categorias (cod_categoria),
    constraint fk_detalles_reference_comproba foreign key (cod_comprobante)
       references efacture_repo.comprobantes (cod_comprobante)
       on delete cascade on update cascade
@@ -161,6 +162,7 @@ create table efacture_repo.usuario_membresia (
    estado_membresia     varchar(20)          not null default 'vigente'
       constraint ckc_estado_membresia_usuario_ check (estado_membresia in ('vigente','no vigente')),
    fecha_vencimiento    timestamp            not null,
+   cant_comprobantes_permitidos int4         not null,
    created_at           timestamp            not null default current_timestamp,
    updated_at           timestamp            null,
    deleted_at           timestamp            null,
@@ -192,13 +194,76 @@ create table efacture_repo.configuracion (
    constraint pk_configuracion primary key (cod_regla)
 );
 
+/*==============================================================*/
+/* Table: usuarios_sesion                                       */
+/*==============================================================*/
+create table efacture_repo.usuarios_sesion (
+   cod_usuario_sesion   serial               not null,
+   cod_usuario          int4                 not null,
+   token_sesion         text                 null
+      constraint ckc_token_sesion_usuarios check (token_sesion is null or (token_sesion in ('cliente','admin'))),
+   intentos_login       int2                 not null default 0,
+   ip_cliente           varchar(50)          not null,
+   created_at           timestamp            not null default current_timestamp,
+   updated_at           timestamp            not null default current_timestamp,
+   deteled_at           timestamp            null,
+   constraint pk_usuarios_sesion primary key (cod_usuario_sesion),
+   constraint fk_usuarios_reference_usuarios foreign key (cod_usuario)
+      references efacture_repo.usuarios (cod_usuario)
+      on delete cascade on update cascade
+);
+
+comment on column efacture_repo.usuarios_sesion.created_at is
+'fecha de intento de sesion fallida o exitosa';
+
+/*==============================================================*/
+/* Table: dataset_entrenamiento                                 */
+/*==============================================================*/
+create table efacture_repo.dataset_entrenamiento (
+   id                   serial               not null,
+   usuario              varchar(13)          not null,
+   fecha                date                 not null,
+   categoria            varchar(50)          not null,
+   monto                numeric(10,2)        not null,
+   created_at           timestamp            not null default current_timestamp,
+   updated_at           timestamp            null,
+   deleted_at           timestamp            null,
+   constraint pk_dataset_entrenamiento primary key (id)
+);
+
+/*==============================================================*/
+/* Table: dataset_entrenado                                     */
+/*==============================================================*/
+create table efacture_repo.dataset_entrenado (
+   id                   serial               not null,
+   usuario              varchar(13)          not null,
+   fecha                date                 not null,
+   anio                 int4                 not null,
+   mes                  int4                 not null,
+   categoria            varchar(50)          not null,
+   monto                numeric(10,2)        not null,
+   created_at           timestamp            not null default current_timestamp,
+   updated_at           timestamp            null,
+   deleted_at           timestamp            null,
+   constraint pk_dataset_entrenado primary key (id)
+);
+
+
 --? usuario admin
 --valores por defecto
 -- clave: Peru321_
 INSERT INTO efacture_repo.usuarios(
-	identificacion, nombres, apellidos, correo, password, tipo_usuario)
-	VALUES ('0602908170', 'darwin', 'bayas', 'tidomarh@hotmail.com', '$pbkdf2-sha256$29000$jXHOuTcG4HxPSan1XiuFEA$JVaoEKE.SpN1PEFJKpyO.YvZGabhPp8P0AXX2mjZ/Zc', 'admin');
+	identificacion, nombres, apellidos, correo, password, tipo_usuario, updated_at)
+	VALUES ('0602908170', 'darwin', 'bayas', 'tidomarh@hotmail.com', '$pbkdf2-sha256$29000$jXHOuTcG4HxPSan1XiuFEA$JVaoEKE.SpN1PEFJKpyO.YvZGabhPp8P0AXX2mjZ/Zc', 'admin', current_timestamp);
+
+INSERT INTO efacture_repo.periodo_fiscal(
+	cod_periodo_fiscal, periodo_fiscal, updated_at)
+	VALUES (1, 9999, current_timestamp);
+
+INSERT INTO efacture_repo.fraccion_basica_desgravada(
+	cod_fraccion_basica, cod_periodo_fiscal, valor_fraccion_basica, updated_at)
+	VALUES (1, 1, 0, current_timestamp);
 
 INSERT INTO efacture_repo.categorias(
-	cod_categoria, categoria, descripcion_categoria, fraccion_basica_desgravada)
-	VALUES (1, 'Desconocido', 'Categoria para detalles de comprobantes con categoria no deducible', 0);
+	cod_categoria, cod_fraccion_basica, categoria, descripcion_categoria, cant_fraccion_basica, updated_at)
+	VALUES (1, 1, 'Desconocido', 'Categoria para detalles de comprobantes con categoria no deducible', 0, current_timestamp);
